41d4a5251a970422bdcd992d6e9f7915d6ffe163
frapastique <frapastique@outlook.com>
2023-01-16 13:20:58 +0100

    commit day 21 (task 8 finished)

--- week05/day21/08_ListeManipulieren.kt
+++ week05/day21/08_ListeManipulieren.kt
@@ -1,4 +1,11 @@
 fun main() {
     var list: MutableList<Int> = mutableListOf(0, 1, 2, 3, 4, 5)
     //Implementiere unter diesem Kommentar die in der Aufgabe beschriebene while-Schleife
+    println(list)
+    var i: Int = 0
+    while (i < list.size) {
+        list.set(i, (list[i].times(2)))
+        i++
+    }
+    println(list)
 }
