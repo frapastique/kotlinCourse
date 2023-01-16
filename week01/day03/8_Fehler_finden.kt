94e47bbb698ffb41a4247b6bd9e464b596a1b751
Gaidis Volkovs <66075561+frapastique@users.noreply.github.com>
2023-01-16 17:27:03 +0100

    Delete 8_Fehler_finden.kt

--- week01/day03/8_Fehler_finden.kt
+++
@@ -1,6 +0,0 @@
-fun main(){
-    var test: String = "Kannst du mich hören?"
-    var test2: String = "Hallo Welt!"
-
-    println("$test2 $test") // <---- Unwichtig, was das bedeutet, hier ist der Fehler nicht zu finden.
-}
