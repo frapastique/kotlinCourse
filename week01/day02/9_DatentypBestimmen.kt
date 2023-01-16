0716abce83a4a1edd47e7a7444dbb46b6762c93a
Gaidis Volkovs <66075561+frapastique@users.noreply.github.com>
2023-01-16 17:26:02 +0100

    Delete 9_DatentypBestimmen.kt

--- week01/day02/9_DatentypBestimmen.kt
+++
@@ -1,6 +0,0 @@
-fun main(){
-    // Setze an der richtigen Stelle den passenden Datentyp für diese Variable ein
-    var productCount: Int = 10
-
-    println("The variable \"$productCount\" is an integer!")
-}
