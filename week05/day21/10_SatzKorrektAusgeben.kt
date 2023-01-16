fc6bfe8dbca127bdf06e706f133693c745f28401
frapastique <frapastique@outlook.com>
2023-01-16 14:01:34 +0100

    commit: day 21 (task 10 finished)

--- week05/day21/10_SatzKorrektAusgeben.kt
+++ week05/day21/10_SatzKorrektAusgeben.kt
@@ -1,10 +1,17 @@
-/*
 fun main(){
-    var umgedrehterSatz = listOf(".", "rückwärts ", "mehr", "nicht", "ist ", "Satz ", "Dieser ")
-    satzAusgabe(umgedrehterSatz)
+    var umgedrehterSatz = listOf(".", "rückwärts", "mehr ", "nicht ", "ist ", "Satz ", "Dieser ")
+    println("Ursprünglicher Satz: $umgedrehterSatz")
+    var lesbarerSatz: MutableList<String> = satzAusgabe(umgedrehterSatz)
+    println("Korrekter Satz:      $lesbarerSatz")
 }
 
-fun satzAusgabe(list: List<String>) : MutableList<String> {
+fun satzAusgabe(list: List<String>): MutableList<String> {
     // hier dein code
-
-}*/
+    var mutableList: MutableList<String> = mutableListOf()
+    var i: Int = list.size - 1
+    while (i >= 0) {
+        mutableList.addAll(listOf(list[i]))
+        i--
+    }
+    return mutableList
+}
