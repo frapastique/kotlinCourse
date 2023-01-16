43a3bd72beaf68f921985f46fb01d40cda56f244
frapastique <frapastique@outlook.com>
2023-01-16 12:18:46 +0100

    initial commit day 21 (task 4 finished)

--- week05/day21/04_SchleifenErgebnisDrucken.kt
+++ week05/day21/04_SchleifenErgebnisDrucken.kt
@@ -17,8 +17,13 @@ fun main() {
     /* FRAGEN:
     * 1. Schaut mit dem Debugger nach, welchen Wert die Variable wirdInSchleifeGedruckt am Ende der Schleife hat.
     * Warum wird nicht dieser Wert ausgegeben?
-    *
+    *       Der Endwert ist vier (4). Dieser wird nicht in der Konsole ausgegeben, weil in der while Schleife <= 3 als
+    *       Bedingung festgelegt wurde. Da 4 größer als 3 ist, zählt dieser als Ausstiegswert (while ist false).
     * 2. Inwiefern unterscheiden sich Schleife 1 und Schleife 2?
-    * */
+    *       In der 1. Schleife ist die Bedingung <= 3 (kleiner gleich 3)
+    *       In der 2. Schleife ist die Bedingung > 3 (größer 3)
+    *       Somit wird in der 2. Schleife erst etwas in der Konsole ausgegeben sobald der Wert größer als 3 ist.
+    *       Also sobald bis 4 aufaddiert wurde wird der Endwert ausgegeben!
+     */
 
 }
