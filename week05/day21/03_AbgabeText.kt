cb7662b28eed0909efd1d9fb1e714715d72f5d20
frapastique <frapastique@outlook.com>
2023-01-16 11:36:55 +0100

    initial commit day 21 (task 3 before test)

--- week05/day21/03_AbgabeText.kt
+++ week05/day21/03_AbgabeText.kt
@@ -1,5 +1,8 @@
 /*
     Schreibe hier deine Antwort auf:
+        0
 
+        Da der Wert 0 nie größer als fünf (5) ist, wird i nicht aufaddiert
+        und gibt den in der Variablen festgelegten Wert von 0 in der Konsole ausgegeben!
 
  */
