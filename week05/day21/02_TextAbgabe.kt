42e805f7283465c0613b3073b186e9dfebdd93e1
frapastique <frapastique@outlook.com>
2023-01-16 11:34:08 +0100

    initial commit day 21 (task 2 finished)

--- week05/day21/02_TextAbgabe.kt
+++ week05/day21/02_TextAbgabe.kt
@@ -1,9 +1,16 @@
 /*
     Schreibe hier deine Antwort auf:
-        0
-        1
-        2
-        3
-        4
+        5
+
+        (fixed) Da in der while Schleife keine println(i) ist, wird nichts ausgegeben bis der Wert bis fünf (5) aufaddiert wurde.
+        Sobald 5 erreicht wird, wird die Zahl fünf (5) in der Konsole ausgegeben!
 
  */
+
+fun main() {
+    var i: Int = 0
+    while (i < 5) {
+        i++
+    }
+    println(i)
+}
