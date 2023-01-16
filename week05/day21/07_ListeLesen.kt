5d829b1f793ce3b087f6b7dd370a14e5bde5f281
frapastique <frapastique@outlook.com>
2023-01-16 12:42:56 +0100

    commit day 21 (task 7 finished)

--- week05/day21/07_ListeLesen.kt
+++ week05/day21/07_ListeLesen.kt
@@ -1,6 +1,9 @@
 fun main () {
     val list : List<String> = listOf("Hallo", "Welt", "!", "Was", "für", "ein", "schöner", "Tag", "!")
     //Erstelle unter diesem Kommentar eine Schleife, die die Liste oben auslesen kann
-
-
+    var i: Int = 0
+    while (i < list.size) {
+        println(list[i])
+        i++
+    }
 }
