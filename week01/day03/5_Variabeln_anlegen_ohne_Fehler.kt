b9719dcf5efd5316656c7817113615d578edd77a
Gaidis Volkovs <66075561+frapastique@users.noreply.github.com>
2023-01-16 17:26:46 +0100

    Delete 5_Variabeln_anlegen_ohne_Fehler.kt

--- week01/day03/5_Variabeln_anlegen_ohne_Fehler.kt
+++
@@ -1,9 +0,0 @@
-fun main(){
-    // Erzeuge eigene Variablen ohne Compilerfehler
-    var zero: Boolean = true
-    var one: Int = 72
-    var two: String = "cookies"
-    var phi: Double = 1.61803398875
-
-    println("The Number Phi equals to $phi, such a beatiful thing! I also like the number $one. Are most of the $two delicious? The answer is: $zero!")
-}
