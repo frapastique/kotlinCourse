1cfce36edcf847269a39097dcdbb1951f022c02c
frapastique <frapastique@outlook.com>
2023-01-17 11:30:27 +0100

    day 22 (task 4 finished)

--- week05/day22/04_ListeLesen.kt
+++ week05/day22/04_ListeLesen.kt
@@ -1,4 +1,13 @@
 fun main() {
     var fruits: List<String> = listOf("Strawberry", "Apple", "Banana", "Lemon")
 
+    for (fruit in fruits) {
+        println(fruit)
+    }
+
+    var i: Int = 0
+    do {
+        println(fruits[i])
+        i++
+    } while (i < fruits.size)
 }
