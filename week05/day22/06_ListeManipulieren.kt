0259df0ecf42f72165ddd818c4526c1c36c6e771
frapastique <frapastique@outlook.com>
2023-01-17 12:35:06 +0100

    day 22 (task 6 finished)

--- week05/day22/06_ListeManipulieren.kt
+++ week05/day22/06_ListeManipulieren.kt
@@ -1,4 +1,11 @@
 fun main() {
     var numbers: MutableList<Int> = mutableListOf(3, 6, 4, 7, 42, 1, 26)
+    println(numbers)
 
+    var numbersPlus5: MutableList<Int> = mutableListOf()
+    for (number in numbers) {
+        numbersPlus5.add(number + 5)
+    }
+    numbers = numbersPlus5
+    println(numbers)
 }
