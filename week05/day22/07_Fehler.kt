806f7e8dc9591f322eba34402946214872a876e2
frapastique <frapastique@outlook.com>
2023-01-17 12:41:15 +0100

    day 22 (task 7 finished)

--- week05/day22/07_Fehler.kt
+++ week05/day22/07_Fehler.kt
@@ -1,10 +1,8 @@
-/*
 fun main() {
     var lieblingsFilme: List<String> = listOf("Batman", "Breaking Bad", "Attack on Titan", "Planet der Affen")
 
     // Verbesser die For-schleife, sodass alle Filme der Liste lieblingsFilme ausgegeben wird.
-    (i in 0..lieblingsFilme.size)
-    println(lieblingsFilme[i])
-
+    for (element in lieblingsFilme) {
+        println(element)
+    }
 }
- */
