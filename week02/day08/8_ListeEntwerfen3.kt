92edbf1e1f26587a11697e78efbe3ce5bf85eb31
Gaidis Volkovs <66075561+frapastique@users.noreply.github.com>
2023-01-18 17:22:08 +0100

    Delete 8_ListeEntwerfen3.kt

--- week02/day08/8_ListeEntwerfen3.kt
+++
@@ -1,6 +0,0 @@
-fun main () {
-    // Entwirf hier eine Liste, welche Tasks abspeichern und nachträglich nicht mehr geändert werden soll
-    // Initial soll die Liste die Werte "Programmieren", "Bugs fixen", "Committen" enthalten.
-    // Dein Code hier:
-    val tasks = listOf<String>("Programmieren", "Bugs fixen", "Committen")
-}
