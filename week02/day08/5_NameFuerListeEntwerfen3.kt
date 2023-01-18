94c2b74b7865ebd0901edde6a7f1aed5e641f083
Gaidis Volkovs <66075561+frapastique@users.noreply.github.com>
2023-01-18 17:21:46 +0100

    Delete 5_NameFuerListeEntwerfen3.kt

--- week02/day08/5_NameFuerListeEntwerfen3.kt
+++
@@ -1,4 +0,0 @@
-fun main () {
-    // Gib der Liste einen passenden Namen. Diese Liste speichert die Zahlen von 0 bis 9.
-    var ganzeZahlen: MutableList<Int> = mutableListOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
-}
