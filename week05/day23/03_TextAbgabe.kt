5c2500aa08f2349143e0a5ec515c206d79819a0f
frapastique <frapastique@outlook.com>
2023-01-18 11:17:59 +0100

    day 23 (task 3)

--- week05/day23/03_TextAbgabe.kt
+++ week05/day23/03_TextAbgabe.kt
@@ -1,4 +1,24 @@
 /*
 Schreibe hier deine Antwort hin.
-
+        Am Montag muss ich arbeiten.
+        Am Dienstag muss ich arbeiten.
+        Am Mittwoch muss ich arbeiten.
+        Am Donnerstag muss ich arbeiten.
+        Am Freitag muss ich arbeiten.
+        Am Samstag hab ich frei.
+        Am Sonntag hab ich frei.
  */
+
+fun main() {
+    val weekDays: List<String> = listOf(
+        "Montag", "Dienstag", "Mittwoch",
+        "Donnerstag", "Freitag", "Samstag", "Sonntag"
+    )
+    for (day in weekDays) {
+        if (day == "Samstag" || day == "Sonntag") {
+            println("Am $day hab ich frei.");
+        } else {
+            println("Am $day muss ich arbeiten.");
+        }
+    }
+}
