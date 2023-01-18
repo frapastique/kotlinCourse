9e5184026564a82275e4d1dfaa9bdfece5f1b6b9
frapastique <frapastique@outlook.com>
2023-01-18 11:45:25 +0100

    day 23 (task 4)

--- week05/day23/04_TextAbgabe.kt
+++ week05/day23/04_TextAbgabe.kt
@@ -1,4 +1,20 @@
 /*
 Schreibe hier deine Antwort hin.
-
+        PH
+        Microsoft
  */
+
+// Anlegen der main Funktion
+fun main() {
+    // Liste brands mit den Marken: "Samsung", "Apple", "PH", "Microsoft"
+    val brands: List<String> = listOf("Samsung", "Apple", "PH", "Microsoft")
+    // for-Schleife innerhalb dessen mit i die Integer 2 und größe der Liste minus eins also 3
+    // nacheinander übergeben werden
+    for (i in 2..brands.size - 1) {
+        // In der Konsole wird der Inhalt der Liste am Index i ausgegeben
+        // beginnend mit Index 2, also PH. Dann der Wert am Index 3, somit Microsoft
+        println(brands[i])
+    }
+    // Sobald über die zwei Zahlen iteriert wurde, bricht die for-Schleife ab.
+}
+// Ende des Programmes
