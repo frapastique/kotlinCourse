3743669990ec98d48fa08e1b47b55a37a5a0a256
Gaidis Volkovs <66075561+frapastique@users.noreply.github.com>
2023-01-18 17:20:01 +0100

    Delete 2_ErstesUndLetztesElement.kt

--- week02/day07/2_ErstesUndLetztesElement.kt
+++
@@ -1,7 +0,0 @@
-fun main() {
-    var liste: MutableList<Int> = mutableListOf(5, 4, 6, 7)
-    // todo: erweitere ab hier den Code.
-    liste.removeFirst()
-    liste.removeLast()
-    println(liste)
-}
