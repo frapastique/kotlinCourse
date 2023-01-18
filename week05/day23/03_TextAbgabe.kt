deb2addafb411221718cb4bcec69a09d488c8180
frapastique <frapastique@outlook.com>
2023-01-18 11:36:43 +0100

    day 23 (task 3 (with comments))

--- week05/day23/03_TextAbgabe.kt
+++ week05/day23/03_TextAbgabe.kt
@@ -9,16 +9,32 @@ Schreibe hier deine Antwort hin.
         Am Sonntag hab ich frei.
  */
 
+// Anlegen der main Funktion
 fun main() {
+    // Anlegen der Liste weekDays mit allen Wochentagen
     val weekDays: List<String> = listOf(
         "Montag", "Dienstag", "Mittwoch",
         "Donnerstag", "Freitag", "Samstag", "Sonntag"
     )
+    // Anlegen einer for Schleife welche über die Liste iteriert und bei jedem Durchlauf einen folgenden
+    // Wochentag in der Variable day weitergibt
     for (day in weekDays) {
+        // Eine if Bedingung mit der Abfrage wenn day gleich Samstag oder day gleich Sonntag
         if (day == "Samstag" || day == "Sonntag") {
+            // Wenn beim Iterieren über die Liste weekDays die Variable day Samstag oder Sonntag übergibt wird:
+            // Am Samstag/Sonntag hab ich frei jeweils einzeln in der Konsole ausgegeben
             println("Am $day hab ich frei.");
+        // else Teil der Bedingung, also wenn day alles außer Samstag/Sonntag übergibt (Montag - Freitag)
         } else {
+            // Trifft dies zu, wird der entsprechende Wochentag (Montag - Freitag) in $day eingesetzt und
+            // Am $day muss ich arbeiten in der Konsole ausgegeben.
             println("Am $day muss ich arbeiten.");
         }
+        // Da die Liste mit Montag beginnt und bis Sonntag alle Wochentage aufzählt, ist die if Bedingung für die ersten
+        // fünf day Übergaben als false gekennzeichnet und else als true. Somit wird erst fünfmal else ausgeführt und
+        // anschließend ist zweimal if Bedingung true (da Samstag, darauf Sonntag aus der Liste übergeben wird) und dann
+        // entsprechend in der Konsole ausgegeben.
     }
+    // Sobald der letzte Eintrag aus der Liste abgerufen wurde, endet die for-Schleife
 }
+// Ende des Programmes
