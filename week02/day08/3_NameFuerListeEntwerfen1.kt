56a04212a565f065e54acb9b6218d76ae2ab4a7b
Gaidis Volkovs <66075561+frapastique@users.noreply.github.com>
2023-01-18 17:21:29 +0100

    Delete 3_NameFuerListeEntwerfen1.kt

--- week02/day08/3_NameFuerListeEntwerfen1.kt
+++
@@ -1,4 +0,0 @@
-fun main () {
-    // Gib der MutableList einen passenderen Namen. Diese Liste speichert Ortsvorwahlen.
-    var ortsVorwahlen: MutableList<String> = mutableListOf("030", "0521", "0851", "0231")
-}
