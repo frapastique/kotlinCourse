de22beb138d7ea0d165d322b98639549f32a1572
frapastique <frapastique@outlook.com>
2023-01-18 14:08:01 +0100

    day 23 (task 9)

--- week05/day23/09_SchleifenUndListen.kt
+++ week05/day23/09_SchleifenUndListen.kt
@@ -1,4 +1,25 @@
+// Programm Start
 fun main() {
+    // Liste mit Strings für einen Satz
     val satz: List<String> = listOf("Tag ", "schöner ", "sehr ", "ein ", "ist ", "Heute ")
+    // Konsolen ausgabe der Liste
+    println(satz)
 
+    var i: Int = satz.size - 1
+    while (i >= 0) {
+        print(satz[i])
+        i--
+    }
+    println("\n")
+
+    for (i in satz.size - 2 downTo 0 step 2) {
+        print(satz[i])
+    }
+    println("\n")
+
+    var j: Int = satz.size -1
+    do {
+        print(satz[j])
+        j--
+    } while (j >= 3)
 }
