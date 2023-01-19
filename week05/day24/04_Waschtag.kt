1057ba3cc6a2b9ea9768e1e8ad1472e01717164f
frapastique <frapastique@outlook.com>
2023-01-19 11:49:45 +0100

    day 24 (task 4)

--- week05/day24/04_Waschtag.kt
+++ week05/day24/04_Waschtag.kt
@@ -3,5 +3,19 @@ fun main() {
         "rot", "blau", "rot", "blau", "blau", "rot", "rot", "blau", "blau",
         "blau", "rot", "blau", "rot", "blau", "blau", "rot", "rot", "blau"
     )
+    println("Insgesamt haben wir ${shirts.size} rote und blaue T-Shirts im Wäschekorb.")
 
+    val redShirts: MutableList<String> = mutableListOf()
+    val blueShirts: MutableList<String> = mutableListOf()
+
+    for (i in shirts) {
+        if (i == "rot") {
+            redShirts.add(i)
+        }
+        if (i == "blau") {
+            blueShirts.add(i)
+            continue
+        }
+    }
+    println("Davon sind ${redShirts.size} rot und ${blueShirts.size} blau.")
 }
