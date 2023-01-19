fb32566f1605adec47a13bdf9ea09ba4e1e59b73
frapastique <frapastique@outlook.com>
2023-01-19 11:35:21 +0100

    day 24 (task 3)

--- week05/day24/03_WhileSchleife.kt
+++ week05/day24/03_WhileSchleife.kt
@@ -1,4 +1,22 @@
 fun main() {
     val numbers: MutableList<Double> = mutableListOf(25.0, 33.3, 50.0, 100.0)
+    println(numbers)
+    println(multiNumbers(7, numbers))
+    var i: Int = 0
+    while (i < numbers.size) {
+        var newNumber: Double = numbers[i].times(3)
+        numbers[i] = newNumber
+        i++
+    }
+    println(numbers)
+}
 
+fun multiNumbers(number: Int, liste: MutableList<Double>): MutableList<Double> {
+    var i: Int = 0
+    for (multi in liste) {
+        var newNumber: Double = liste[i].times(number)
+        liste[i] = newNumber
+        i++
+    }
+    return liste
 }
