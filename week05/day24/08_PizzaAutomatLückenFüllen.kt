1af0a9ef0294bf1461143fed1967756c89000c87
frapastique <frapastique@outlook.com>
2023-01-19 15:34:26 +0100

    day 24 (task 8 finished)

--- week05/day24/08_PizzaAutomatLückenFüllen.kt
+++ week05/day24/08_PizzaAutomatLückenFüllen.kt
@@ -1,4 +1,3 @@
-/*
 fun main(){
 
     println("Willkommen beim Pizza-Automat.")
@@ -7,7 +6,7 @@ fun main(){
 
     var zutatenListe: List<String> = listOf("Mehl", "Hefe", "Wasser", "Tomatensoße", "Käse")
     var a = 1
-    for (){
+    for (i in zutatenListe.indices){
         println("$a. Grundzutat: ${zutatenListe[i]}")
         a++
     }
@@ -16,7 +15,7 @@ fun main(){
 
     for (i in zutatenListe.indices){
         if (i in 0..2){
-            print("${}}, ")
+            print("${zutatenListe[i]}, ")
         }
     }
     println("werden in einer Schüssel vermischt und geknetet....")
@@ -24,8 +23,8 @@ fun main(){
     println("...")
     println("Pizzateig erstellt.")
 
-    for (_ in zutatenListe.indices){
-        if (i in _..zutatenListe.size){
+    for (i in zutatenListe.indices){
+        if (i in 3..zutatenListe.size){
             print("${zutatenListe[i]}, ")
         }
     }
@@ -34,7 +33,7 @@ fun main(){
     println("Gib jetzt einen Belag ein, den du auf der Pizza sehen willst.")
     var ersterBelag = readln()
     println("Gib bitte einen weiteren Belag ein, den du auf der Pizza sehen willst.")
-    readln()
+    var zweiterBelag = readln()
     println("Gib den letzten Belag ein, den du auf der Pizza sehen willst.")
     var dritterBelag = readln()
 
@@ -43,7 +42,7 @@ fun main(){
     println("Alles klar!")
     println("Du hast folgende Beläge ausgewählt:")
     var b = 1
-    for (i in ){
+    for (i in belagListe.indices){
         println("$b. Belag: ${belagListe[i]}")
         b++
     }
@@ -54,7 +53,7 @@ fun main(){
     println("Jetzt muss die Pizza für 10 Minuten in den Ofen. Der Countdown läuft!")
 
     for (i in 10 downTo 1){
-        if (___){
+        if (i > 1){
             println("$i Minuten noch...")
         } else {
             println("$i Minute noch...")
@@ -63,4 +62,3 @@ fun main(){
     }
     println("FERTIG! Genieß die Pizza :P")
 }
-*/
