25e49492d1031fe51198f29361ef9f65a193dcfa
frapastique <frapastique@outlook.com>
2023-01-19 11:15:05 +0100

    day 24 (task 2)

--- week05/day24/02_Continue.kt
+++ week05/day24/02_Continue.kt
@@ -1,4 +1,11 @@
 fun main() {
     val numbers: List<Int> = listOf(34, 42, 235, 42, 42, 24, 42, 42, 7346, 34)
 
+    for (i in numbers) {
+        if (i == 42) {
+            continue
+        } else {
+            println(i)
+        }
+    }
 }
