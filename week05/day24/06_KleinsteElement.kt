3a77d455e7f1642aa82fc768b23ee930616fa48b
frapastique <frapastique@outlook.com>
2023-01-19 12:25:37 +0100

    day 24 (task 6)

--- week05/day24/06_KleinsteElement.kt
+++ week05/day24/06_KleinsteElement.kt
@@ -4,4 +4,22 @@ fun main() {
         52315, 346, 3567, 346, 235, 26, 357, 486, 457, 246, 246, 3247,
         35, 247, 2437, 274, 374, 35, 634, 35, 37, 2436, 43
     )
+    println(numbers)
+    println("Zur Kontrolle: ${numbers.min()}")
+    var smallest: Int = smallestNumber(numbers)
+    if (numbers.min() == smallest) {
+        println("Die Zahl '$smallest' ist kleinste Zahl der Liste.")
+    } else {
+        println("Die Zahl '$smallest}' ist nicht die kleinste Zahl der Liste.")
+    }
+}
+
+fun smallestNumber(liste: List<Int>): Int {
+    var smallest: Int = liste[0]
+    for (i in liste.indices) {
+        if (smallest > liste[i]) {
+            smallest = liste[i]
+        }
+    }
+    return smallest
 }
