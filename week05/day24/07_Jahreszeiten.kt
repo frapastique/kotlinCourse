3113f203eabf4df9576335c75cc63357bfcf057d
frapastique <frapastique@outlook.com>
2023-01-19 12:33:44 +0100

    day 24 (task 7a)

--- week05/day24/07_Jahreszeiten.kt
+++ week05/day24/07_Jahreszeiten.kt
@@ -3,4 +3,23 @@ fun main() {
         "Januar", "Februar", "März", "April", "Mai", "Juni",
         "Juli", "August", "September", "Oktober", "November", "Dezember"
     )
+    println(monate)
+
+    for (i in monate) {
+        if (i == "Januar" || i == "Februar" || i == "Dezember") {
+            println("Der $i ist im Winter.")
+            continue
+        } else if (i == "März" || i == "April" || i == "Mai") {
+            println("Der $i ist im Frühling.")
+            continue
+        } else if (i == "Juni" || i == "Juli" || i == "August") {
+            println("Der $i ist im Sommer.")
+            continue
+        } else if (i == "September" || i == "Oktober" || i == "November") {
+            println("Der $i ist im Herbst.")
+            continue
+        } else {
+            break
+        }
+    }
 }
