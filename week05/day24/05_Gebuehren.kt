3daa66f6e4bd4cb9443f82bf1911a457f2ccc640
frapastique <frapastique@outlook.com>
2023-01-19 12:00:19 +0100

    day 24 (task 5)

--- week05/day24/05_Gebuehren.kt
+++ week05/day24/05_Gebuehren.kt
@@ -1,4 +1,13 @@
 fun main() {
     val fees = listOf(363.34, 235.45, 753.45, 234.56, 658.33, 22.65, 2623.46, 475.54)
+    println("Zur Kontrolle: ${fees.sum()}")
+    println("Der Chef muss insgesamt ${sumFees(fees)}€ Gebühren bezahlen.")
+}
 
+fun sumFees(feeList: List<Double>): Double {
+    var totalFees: Double = 0.00
+    for (i in feeList.indices) {
+        totalFees += feeList[i]
+    }
+    return totalFees
 }
