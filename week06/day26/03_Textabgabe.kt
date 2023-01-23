16017cbdb13045551f41b4fa133ba93975a8389f
frapastique <frapastique@outlook.com>
2023-01-23 11:54:28 +0100

    day 26 (task 3 formatting)

--- week06/day26/03_Textabgabe.kt
+++ week06/day26/03_Textabgabe.kt
@@ -1,14 +1,35 @@
 /*
-Antworten:
-Frage 1:
 
-Frage 2:
+Frage 1 (Klasse/Instanz):
+    1. Eine Klasse ist wie ein Bauplan.
+    2. Von einer Klasse kann ich beliebig viele Objekt-Instanzen erstellen.
+    3. Von einer Klasse kann ich genau eine Objekt-Instanz erstellen.
 
-Frage 3:
+Antwort 1:
 
-Frage 4:
 
-Frage 5:
+Frage 2 (Punktnotation):
+    1. Punktnotation wird bei der Benennung von Objekt-Instanzen benutzt.
+    2. Mit Punktnotation kann man auf die Methoden eines Objekts zugreifen.
+    3. Punktnotation ist ein Datentyp, der für das Anlegen von Klassen verwendet wird.
+
+Antwort 2:
+
+
+Frage 3 (Eigenschaften):
+    1. Eigenschaften werden als Funktionen in einer Klasse geschrieben.
+    2. Eigenschaften sind immer Konstanten in einer Klasse.
+    3. Eigenschaften werden als Variablen/Konstanten in einer Klasse geschrieben.
+
+Antwort 3:
+
+
+Frage 4 (Methoden):
+    1. Methoden von einer Objekt-Instanz haben Zugriff auf seine Eigenschaften.
+    2. Methoden sind unabhängig von Klassen.
+    3. Methoden werden als Funktionen innerhalb einer Klasse geschrieben.
+
+Antwort 4:
 
 
  */
