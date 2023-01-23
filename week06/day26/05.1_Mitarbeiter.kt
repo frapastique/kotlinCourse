6ff10358af5f49926daaa4c036568c7f9e301395
frapastique <frapastique@outlook.com>
2023-01-23 12:54:05 +0100

    day 26 (task 5b)

--- week06/day26/05.1_Mitarbeiter.kt
+++ week06/day26/05.1_Mitarbeiter.kt
@@ -1,3 +1,27 @@
 class Mitarbeiter(id: Int, name: String, position: String, gehalt: Double, artDerBeschaeftigung: String, mutterschaftsUrlaub: Boolean) {
 
+    var id: Int = 0
+    var name: String = ""
+    var position: String = ""
+    var gehalt: Double = 0.00
+    var artDerBeschaeftigung: String = ""
+    var mutterschaftsUrlaub: Boolean = true
+
+    fun gehaltAusgeben() {
+        println("Name:   $name")
+        println("Gehalt: $gehalt")
+    }
+
+    fun befoerdern(neuePosition: String, neuerGehalt: Double) {
+        position = neuePosition
+        gehalt = neuerGehalt
+    }
+
+    fun istVerfuegbar() {
+        if (true) {
+            println("Mitarbeiter:in ist gerade im Mutterschaftsurlaub.")
+        } else {
+            println("Mitarbeiter:in ist Verfügbar.")
+        }
+    }
 }
