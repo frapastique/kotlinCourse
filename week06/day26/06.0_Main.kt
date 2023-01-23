e24f2d289ff900531c5b4aada3a746362ccdeb9f
frapastique <frapastique@outlook.com>
2023-01-23 15:55:16 +0100

    day 26 (task 6)

--- week06/day26/06.0_Main.kt
+++ week06/day26/06.0_Main.kt
@@ -3,4 +3,11 @@ fun main() {
     //Nutze die einlesen-Methode, um den Zaehler und Nenner einzugeben
     //Nutze die ausgeben-Methode, um den Bruch wieder auszugeben
     //Nutze die wert-Methode, um den Bruch in eine Kommazahl umzuwandeln
+    /*
+    Bruch().einlesen()
+    Bruch().ausgeben()
+    Bruch().wert()
+    Bruch().kehrwert()
+    */
+    Bruch().alles()
 }
--- week06/day26/06.1_Bruch.kt
+++ week06/day26/06.1_Bruch.kt
@@ -4,4 +4,71 @@ class Bruch {
     var nenner: Int = 1
 
     //Hier drunter kannst du weitercoden:
+    fun alles() {
+        einlesen()
+        println("\nRechnung:")
+        println("$zaehler/$nenner = ${wertRechnung()}")
+        println("\nKehrwert:")
+        println("$nenner/$zaehler = ${kehrwertRechnung()}")
+    }
+
+    fun ausgeben() {
+        println("$zaehler/$nenner\n")
+    }
+
+    fun setze(z: Int, n: Int) {
+        if (z != 0) {
+            zaehler = z
+            if (n != 0) {
+                nenner = n
+            }
+        }
+    }
+
+    fun einlesen() {
+        try {
+            print("\nGebe den Zähler ein: ")
+            var number0 = readln().toInt()
+            while (true) {
+                if (number0 > 0) {
+                    try {
+                        print("\nGebe den Nenner ein: ")
+                        var number1 = readln().toInt()
+                        while (true) {
+                            if (number1 > 0) {
+                                return setze(number0, number1)
+                            } else {
+                                println("\nBitte gebe eine Zahl über 0 ein.")
+                                einlesen()
+                            }
+                        }
+                    } catch (e: Exception) {
+                        println("\nBitte gebe eine ganze Zahl ein.")
+                        einlesen()
+                    }
+                } else {
+                    println("\nBitte gebe eine Zahl über 0 ein.")
+                    einlesen()
+                }
+            }
+        } catch (e: Exception) {
+            println("\nBitte gebe eine ganze Zahl ein.")
+            einlesen()
+        }
+    }
+
+    fun wert() {
+        println(zaehler.toDouble().div(nenner.toDouble()))
+        println()
+    }
+    fun wertRechnung(): Double {
+        return zaehler.toDouble().div(nenner.toDouble())
+    }
+
+    fun kehrwert() {
+        println("$nenner/$zaehler\n")
+    }
+    fun kehrwertRechnung(): Double{
+        return nenner.toDouble().div(zaehler.toDouble())
+    }
 }
