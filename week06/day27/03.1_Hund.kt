d821fa87d37253fffdce6e23c79ae39075bebceb
frapastique <frapastique@outlook.com>
2023-01-24 12:19:08 +0100

    day 27 (task 3a)

--- week06/day27/03.1_Hund.kt
+++ week06/day27/03.1_Hund.kt
@@ -1,2 +1,9 @@
-class Hund {
+class Hund(var name: String, var age: Int, var race: String) {
+    fun bellen() {
+        println("woof woof")
+    }
+    fun aging() {
+        age++
+        println("Unser Hund $name der Rasse $race wird heute $age Jahre alt")
+    }
 }
