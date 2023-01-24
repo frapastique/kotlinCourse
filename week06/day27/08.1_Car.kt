fcc3675cc2b1fa963953c841eb993da48c0bb3e8
frapastique <frapastique@outlook.com>
2023-01-24 13:35:56 +0100

    day 27 (task 8a)

---
+++ week06/day27/08.1_Car.kt
@@ -0,0 +1,15 @@
+class Auto {
+    var ps: Int
+    var color: String
+    var seats: Int
+    var freeSeats: Int
+
+    constructor(ps: Int, color: String, seats: Int, freeseats: Int) {
+        this.ps = ps
+        this.color = color
+        this.seats = seats
+        this.freeSeats = freeseats
+    }
+
+
+}
