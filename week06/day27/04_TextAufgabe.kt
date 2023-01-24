5d164d3beb4fa57448f663812e3b7bc937b4a560
frapastique <frapastique@outlook.com>
2023-01-24 13:04:57 +0100

    day 27 (task 4 started)

--- week06/day27/04_TextAufgabe.kt
+++ week06/day27/04_TextAufgabe.kt
@@ -1,4 +1,34 @@
 /*
-Schreibe hier deine Antwort auf:
+Beschreibe Zeile für Zeile, was dieser Code macht:
 
+class GameCharacter {
+    var name: String
+    var hasWeapon: Boolean
+    var level: Int
+
+    constructor(name: String, hasWeapon: Boolean, level: Int) {
+        this.name = name
+        this.hasWeapon = hasWeapon
+        this.level = level
+    }
+}
+
+    4.  Klasse GameCharacter wird erstellt.
+    5.  Variable name wird als String festgelegt
+    6.  Variable hasWeapon wird als Boolean festgelegt
+    7.  Variable level wird als Integer festgelegt
+    9.  Eigenschaften des Konstruktors werden mit name als String, hasWeapon als Boolean und level als Integer festgelegt
+    10. Dieser name wird nun name zugeschrieben
+    10. Dieser hasWeapon wird nun hasWeapon zugeschrieben
+    10. Dieser level wird nun level zugeschrieben
+
+    var gameCharacter1: GameCharacter = GameCharacter("Legolas", false, 20)
+    var gameCharacter2: GameCharacter = GameCharacter("Frodo", true, 3)
+
+    println(gameCharacter1.name + " ist Level " + gameCharacter1.level +
+              ". Hat er eine Waffe? " + gameCharacter1.hasWeapon)
+    println(gameCharacter2.name + " ist Level " + gameCharacter2.level +
+              ". Hat er eine Waffe? " + gameCharacter2.hasWeapon)
+
+    25.
  */
