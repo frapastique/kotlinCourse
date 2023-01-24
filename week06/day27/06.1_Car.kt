f4ecbc6ee99abc6c33e8d6eec0d0f3a875e6eaa8
frapastique <frapastique@outlook.com>
2023-01-24 13:09:41 +0100

    day 27 (task 6)

--- week06/day27/06.1_Car.kt
+++ week06/day27/06.1_Car.kt
@@ -1,9 +1,11 @@
 class Car {
     var color: String
     var brand: String
+    var speed: String
 
-    constructor(color: String, brand: String) {
+    constructor(color: String, brand: String, speed: String) {
         this.color = color
         this.brand = brand
+        this.speed = speed
     }
 }
