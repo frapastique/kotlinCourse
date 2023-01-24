27460c7c07113b894e7532c024808feda926cc39
frapastique <frapastique@outlook.com>
2023-01-24 13:51:24 +0100

    day 27 (task 8b)

--- week06/day27/08.1_Car.kt
+++ week06/day27/08.1_Car.kt
@@ -11,5 +11,37 @@ class Auto {
         this.freeSeats = freeseats
     }
 
+    fun getIn() {
+        if (freeSeats > 0) {
+            freeSeats--
+            if (freeSeats == 0) {
+                println("Das Auto ist nun mit $seats Peronen voll besetzt.")
+            } else {
+                println("Das Auto hat noch $freeSeats von $seats Sitzplätzen frei.")
+            }
+        } else {
+            println("Das Auto ist mit $seats Personen voll besetzt und hat keine freien Plätze mehr.")
+        }
+    }
 
+    fun getOut() {
+        if (freeSeats < seats) {
+            freeSeats++
+            if (freeSeats == seats) {
+                println("Das Auto ist leer.")
+            } else {
+                println("Das Auto hat nun $freeSeats von $seats freie Sitzplätze.")
+            }
+        } else {
+            println("Im Auto sitzt niemand und hat $freeSeats freie Sitzplätze.")
+        }
+    }
+
+    fun startEngine() {
+        if (seats > freeSeats) {
+            println("wroom wroom")
+        } else {
+            println("Das Auto ist leer und wird somit nicht gestartet.")
+        }
+    }
 }
