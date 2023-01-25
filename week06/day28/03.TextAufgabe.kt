3306fe2ffc0d1e2dcf10d87c05dc6482f2fe5e6e
frapastique <frapastique@outlook.com>
2023-01-25 11:48:26 +0100

    day 28 (task 3)

---
+++ week06/day28/03.TextAufgabe.kt
@@ -0,0 +1,40 @@
+/*
+class Person {
+  var age: Int
+
+  constructor(age: Int) {
+    this.age = age
+  }
+
+  fun birthday(){
+    age = age + 1
+  }
+
+  fun isUnderage() : Boolean {
+    return(age < 18)
+  }
+}
+
+Hier wird die Klasse "Person" mit der Variable "age" als Integer angelegt.
+Zudem besitzt diese die Methoden "birthday" und "isUnderage".
+
+Im Konstruktor wird der Klassen Variable "age" übergeben.
+
+In der Methode "birthday" wird die Eigenschaft age um 1 erhöht.
+
+In der Methode "isUnderage" wird, wenn die Eigenschaft "age" unter 18 ist
+der Boolean true zurückgegeben.
+
+var person = Person(15)
+person.birthday()
+println(person.isUnderage())
+
+Innerhalb diesen Codeabschnitts wird die Variable "person" in der Klasse "Person"
+mit der Eigenschaft "age" 15 initialisiert.
+
+Darauf wird auf "person" die Methode "birthday" angewendet, welche die Eigenschaft
+"age" um 1 erhöht.
+
+Nun wird auf "person" die Methode "isUnderage" angewendet, welche "true" in der Konsole
+ausgibt. (Da die "person" unter 18 ist)
+ */
