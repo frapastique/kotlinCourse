957668fda8be0c10b8984d53eb47886c8e4917f8
frapastique <frapastique@outlook.com>
2023-01-25 11:17:22 +0100

    day 28 (task 1)

--- week06/day28/01_TextAbgabe.kt
+++ week06/day28/01_TextAbgabe.kt
@@ -1,3 +1,63 @@
 /*
+class Car {
+  var color: String
+  var speed: Int
 
+  constructor(color: String, speed: Int) {
+    this.color = color
+    this.speed = speed
+  }
+
+  fun paint(color: String) {
+    this.color = color
+  }
+
+  fun tune() {
+    speed = speed + 10
+  }
+}
+
+Dieser Codeschnipsel definiert eine Klasse namens "Car".
+Die Klasse hat zwei Eigenschaften, "color" und "speed",
+die als Variablen deklariert sind. Außerdem gibt es zwei Methoden
+in der Klasse, "paint" und "tune".
+
+Der Konstruktor der Klasse nimmt zwei Parameter entgegen,
+"color" und "speed", und initialisiert die entsprechenden
+Eigenschaften der Klasse mit diesen Werten.
+
+Die Methode "paint" nimmt einen Parameter namens "color" entgegen
+und setzt die Eigenschaft "color" der Klasse auf den übergebenen Wert.
+
+Die Methode "tune" erhöht die Eigenschaft "speed" um 10.
+
+Zusammenfassend lässt sich sagen, dass dieses Codeschnipsel eine Klasse
+"car" definiert, die die Eigenschaften "color" und "speed" besitzt und
+zwei Methoden zum Ändern dieser Eigenschaften bereitstellt.
+
+var car: Car = Car("weiss", 150)
+
+car.paint("schwarz")
+car.tune()
+
+println(car.color)
+println(car.speed)
+
+Dieses Codeschnipsel erstellt eine Variable namens "car" und
+initialisiert sie mit einem neuen Objekt der Klasse "Car". Der Konstruktor
+der Klasse wird mit den Werten "white" für die Farbe und 150
+für die Geschwindigkeit aufgerufen.
+
+Dann wird die Methode "paint" auf das Objekt "car" angewendet und die Farbe
+in "black" geändert. Dann wird die Methode "tune" auf das "car"-Objekt
+angewandt, wodurch die Geschwindigkeit um 10 erhöht wird.
+
+Schließlich werden die Eigenschaften "Farbe" und "Geschwindigkeit" des
+Objekts "car" mit "println" auf der Konsole ausgegeben. Das Ergebnis
+wäre "schwarz" und "160".
+
+Zusammenfassend lässt sich sagen, dass dieses Codeschnipsel ein Objekt
+der Klasse "car" erzeugt, die Farbe und die Geschwindigkeit mit den
+Methoden "paint" und "tune" ändert und die aktualisierten Eigenschaften
+auf der Konsole ausgibt.
  */
