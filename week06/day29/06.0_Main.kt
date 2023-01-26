fb6f64366f9bb7b88d1e49f782e13fe77f28c7e0
frapastique <frapastique@outlook.com>
2023-01-26 17:33:42 +0100

    day 29 (task 6; main add pokemon, Pokemon add base)

--- week06/day29/06.0_Main.kt
+++ week06/day29/06.0_Main.kt
@@ -1,4 +1,6 @@
 fun main(){
+    var bulbasaur: Pokemon = Pokemon("Bulbusaur", 3, 29, "grass")
+    var charmander: Pokemon = Pokemon("Charmander", 3, 29, "fire")
+    var squirtle: Pokemon = Pokemon("Squirtle", 3, 29, "water")
 
 }
-
--- week06/day29/06.1_Pokemon.kt
+++ week06/day29/06.1_Pokemon.kt
@@ -1,3 +1,19 @@
-open class Pokemon() {
+open class Pokemon(
+    var name: String,
+    var level: Int,
+    var hitPoints: Int,
+    var type: String) {
 
+    var damageValue = 0.5 * level
+    var isKO = false
+
+    fun looseHP(lost: Int) {
+        hitPoints -= lost
+        if (hitPoints == 0) {
+            isKO = true
+        }
+    }
+    open fun attack(pok: Pokemon) {
+
+    }
 }
