61482439c45e420f5c3c5a82269d6cc35b74aefd
frapastique <frapastique@outlook.com>
2023-01-27 11:28:18 +0100

    day 30 (class Tier add methods)

--- week06/day30/Tier.kt
+++ week06/day30/Tier.kt
@@ -1,3 +1,15 @@
-open class Tier(val name: String, val weight: Double, val age: Int, val gender: String) {
-
+open class Tier(val name: String, var weight: Double, val age: Int, val gender: String) {
+    fun move() {
+        println("Die $name hat sich bewegt.")
+    }
+    open fun animalNoises() {
+        println("Tier geräusch:")
+    }
+    fun petting(visitorName: String) {
+        println("$visitorName streichelt ${this.name}.")
+    }
+    fun feed(visitorName: String) {
+        weight = weight.times(1.02)
+        println("$visitorName füttert ${this.name} und wiegt nun ${this.weight}.")
+    }
 }
