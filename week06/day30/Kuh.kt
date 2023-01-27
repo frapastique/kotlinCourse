9e7be9d3c79a92bf04d25ada42ecdf1e9c8f2ed3
frapastique <frapastique@outlook.com>
2023-01-27 16:32:36 +0100

    day 30 (small fixes)

--- week06/day30/Kuh.kt
+++ week06/day30/Kuh.kt
@@ -3,8 +3,10 @@ class Kuh(name: String, weight: Double, age: Int, gender: String, val producesMi
         println("${this.name}: Muuuh")
     }
     fun milking(nameVisitor: String) {
-        if (producesMilk) {
+        if (producesMilk && age > 3) {
             println("$nameVisitor melkt die Kuh ${this.name}.")
+        } else if (producesMilk && age <= 3) {
+            println("Achtung $nameVisitor! Die Kuh ${this.name} ist mit ${this.age} Jahren zu jung um Milch zu produzieren.")
         } else {
             println("Achtung $nameVisitor! Die Kuh ${this.name} ist ${this.gender} und gibt keine Milch.")
         }
