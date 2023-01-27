ea5c4926f5da8f71207f41c0b50c09ae754d2983
frapastique <frapastique@outlook.com>
2023-01-27 12:06:47 +0100

    day 30 (main test class Pony)

--- week06/day30/Main.kt
+++ week06/day30/Main.kt
@@ -1,10 +1,12 @@
 fun main () {
     val visitor0 = Besucher("Franz", 35)
+    val visitor1 = Besucher("Sissi", 4)
 
     val animal0 = Tier("Helga", 35.3, 12, "weiblich")
     val sheep0 = Schaf("Shawn", 23.4, 5, "weiblich")
     val hen0 = Huhn("Kikeri", 2.5, 2, "weiblich", 2)
     val cow0 = Kuh("Berta", 525.0, 7, "weiblich", true)
+    val pony0 = Pony("Gustav", 315.0, 3, "männlich", 30.5)
 
     visitor0.introduction()
 
@@ -24,4 +26,7 @@ fun main () {
     println(hen0.egsPerDay)
 
     cow0.milking(visitor0.name)
+
+    pony0.riding(visitor0.name, visitor0.age)
+    pony0.riding(visitor1.name, visitor1.age)
 }
