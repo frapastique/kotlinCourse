70b3ac7522b6d958d259dbfd57239235d4b6305c
frapastique <frapastique@outlook.com>
2023-01-27 11:16:24 +0100

    day 30 (class Tier && main test)

--- week06/day30/Main.kt
+++ week06/day30/Main.kt
@@ -1,5 +1,12 @@
 fun main () {
-    val besucher0: Besucher = Besucher("Franz", 35)
+    val visitor0 = Besucher("Franz", 35)
 
-    besucher0.introduction()
+    val animal0 = Tier("Helga", 35.3, 12, "weiblich")
+
+    visitor0.introduction()
+
+    println(animal0.name)
+    println(animal0.weight)
+    println(animal0.age)
+    println(animal0.gender)
 }
---
+++ week06/day30/Tier.kt
@@ -0,0 +1,3 @@
+open class Tier(val name: String, val weight: Double, val age: Int, val gender: String) {
+
+}
