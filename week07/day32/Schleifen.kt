7ffe58f90e2bbc90ff0d6223053991e7c45e356a
frapastique <frapastique@outlook.com>
2023-01-31 11:43:30 +0100

    day 32 (initial commit)

---
+++ week07/day32/Schleifen.kt
@@ -0,0 +1,58 @@
+fun main() {
+
+    /* TODO: Gib die Zahlen von 1 bis 30 mit einer schleife aus. */
+
+
+    /* TODO: Gib jede dritte Zahl von 1 bis 30 mit einer schleife aus. */
+
+
+    /* TODO: Gib die Zahlen von 1 bis 30 mit einer schleife rückwärts aus. */
+
+
+    /* TODO: Gib jede 7. Zahl von 1 bis 30 mit einer schleife rückwärts aus. */
+
+
+    /* TODO: Verwende eine Schleife, um alle Elemente der Liste auszugeben. */
+    val liste1 = listOf("Ich ", "bin ", "ein ", "kleiner ", "Kaktus.")
+
+
+    /* TODO: Verwende eine Schleife, um alle Elemente der Liste rückwärts auszugeben. */
+    val liste2 = listOf("Kaktus.", "kleiner ", "ein ", "bin ", "Ich ")
+
+
+    /* TODO: Verwende eine Schleife, um die Anzahl der Elemente einer Liste zu zählen. */
+    val liste3 = listOf(
+        "Mein", "kleiner", "grüner", "Kaktus", "Steht", "draußen", "am", "Balkon",
+        "Hollari", "hollari", "hollaro", "Was", "brauch'", "ich", "rote", "Rosen?",
+        "Was", "brauch'", "ich", "roten", "Mohn?", "Hollari", "hollari", "hollaro",
+    )
+
+
+    /* TODO: Verwende eine Schleife, um die Zahlen in einer Liste zu summieren. */
+    val liste4 = listOf(23, 465, 456, 24, 46, 2, 7, 79, 2435, 1, 564, 4, 46, 568, 5, 234, 23, 5)
+
+
+    /* TODO: Verwende eine Schleife, um die Anzahl der Zahl 42 in der Liste zu zählen. */
+    val liste5 = listOf(23, 465, 42, 456, 24, 42, 46, 2, 7, 79, 2435, 1, 564, 42, 4, 46, 568, 5, 42, 234, 23, 5)
+
+
+    /* TODO: Verwende eine Schleife, um die Position (= den Index) eines Elements einer Liste zu finden
+    * Gesucht wird der Index des Strings "Balkon".
+    * */
+    val liste6 = listOf(
+        "Mein", "kleiner", "grüner", "Kaktus", "Steht", "draußen", "am", "Balkon",
+        "Hollari", "hollari", "hollaro", "Was", "brauch'", "ich", "rote", "Rosen?",
+    )
+
+
+    /* TODO: Verwende eine Schleife, um alle Schlüssel für einen gegebenen Wert in einer Map zu finden
+    * Gesucht werden alle Schlüssel, die auf "Hallo" zeigen.
+    * */
+    val map = mapOf(
+        42.23 to "Hallo",
+        32.53 to "Welt",
+        23.43 to "Hallo",
+        62.28 to "Haus",
+        16.96 to "Hallo",
+    )
+}
